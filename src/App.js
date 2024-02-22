a601833af26b01d66d45374ca95c405bf52d65e2
Nico Gambolati <nicolasgambolati@gmail.com>
2024-02-22 12:50:55 -0300

    removes item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-// import './App.scss';
+// import './App.css';
+import './App.scss';
 import Data from './components/Data';
 import Header from './components/Header';
+import Tasks from "./components/Tasks";
+
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <Data />
+        {/* <Data /> */}
+        <Tasks />
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,16 +1,52 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from "react";
 
-export class Tasks extends Component {
-  render() {
-    return (
-      <div className="wrapper-component">
-        <h3 className="d-flex title-page">Mis tareas</h3>
-        <button className="btn btn-primary " onClick={''}>
-              Añadir tarea
-          </button>
+function Tasks() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleDelete = (item) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${item.id}`, {
+      method: 'DELETE',
+    })
+    .then(response => {
+      if (response.ok) {
+        setData(oldValues => oldValues.filter(oldValue => oldValue.id !== item.id));
+      } else {
+        throw new Error('Failed to delete the item');
+      }
+    })
+    .catch(error => console.error(error));
+  };
+
+  return (
+    <div className="wrapper-component">
+      <h3 className="d-flex title-page">Mis tareas</h3>
+
+      <div>
+        {data ? (
+          <div>
+              {data.map((item) => (
+                <div className="card" key={item.id}>
+                  <div className="card-body item-title">{item.title}</div>
+                  <button onClick={() => handleDelete(item)}>Borrar</button>
+                </div>
+              ))}
+          </div>
+        ) : (
+          <p>Loading data...</p>
+        )}
       </div>
-    )
-  }
+      <button className="btn btn-primary " onClick={""}>
+        Añadir tarea
+      </button>
+    </div>
+  );
 }
 
-export default Tasks
+export default Tasks;
