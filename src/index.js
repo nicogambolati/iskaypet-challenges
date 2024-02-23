bab37e974ada06df4512fe071c58f1a4900d58f6
Nico Gambolati <nicolasgambolati@gmail.com>
2024-02-22 21:07:11 -0300

    fixes minor styling errors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
